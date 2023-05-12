be4d7b33f88e056ac7acbd900ffa7970276e66f8
Jesus Borjas <jesusbor006@gmail.com>
2023-05-12 18:00:57 -0500

    Add navbar with styling and logo

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-
+import Header from "../Header/Header";
 
 const App = () => {
   return (
     <div className="App">
-
+      <Header />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,3 +1,12 @@
-const Header = () => {};
+import "./Header.css";
+import logo from "../../images/tldr_logo.svg";
+
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <img className="logo-img" src={logo} alt="logo" />
+    </nav>
+  );
+};
 
 export default Header;
