e9a615c0931f0f216846b3ea34f5ec8c0913f616
Jesus Borjas <jesusbor006@gmail.com>
2023-05-12 18:20:56 -0500

    Add Form container with styling

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import Header from "../Header/Header";
+import Form from "../Form/Form";
+
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <main className="main-content">
+        <h1 className="heading">Terms of Service Processor</h1>
+        <p className="sub-heading">Understand what's important to you.</p>
+        <Form />
+
+      </main>
     </div>
   );
 };
