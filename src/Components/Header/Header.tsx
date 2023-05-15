760cb556fff9df64062132210dd556307de7796a
Jason Kirchman <kirchman.jason@icloud.com>
2023-05-15 12:48:24 -0500

    More seafoam css

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import "./Header.css";
-import logo from "../../images/tldr_logo.svg";
+import logo_sm from "../../images/tldr_sm_dark.svg";
+import UserIcon from "../../images/user.jpeg";
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Header = () => {
   return (
-    <nav className="navbar">
-      <img className="logo-img" src={logo} alt="logo" />
-    </nav>
+    <header>
+      <img className="logo-img" src={logo_sm} alt="logo" />
+      <div className="user-div">
+        <IconButton aria-label="delete">
+            <LogoutIcon />
+        </IconButton>
+        <Avatar alt="Remy Sharp" src={UserIcon} />
+      </div>
+    </header>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main:'#0D4C92'
+      },
+      secondary: {
+        main: '#59C1BD'
+      }
+    }
+  });
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
