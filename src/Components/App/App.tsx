0f610dee2376c1e41014320e40b4d0c045463b7c
Jesus Borjas <jesusbor006@gmail.com>
2023-05-17 11:16:00 -0500

    Feat: Get rid of commented out code and unused imports

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -4,31 +4,27 @@ import Header from "../Header/Header";
 import Form from "../Form/Form";
 import Results from "../Results/Results";
 import { ConcernsShape } from "../../interfaces";
-import Login from "../Login/Login";
 
 const App = () => {
   const [error, setError] = useState("");
   const [concerns, setConcerns] = useState<ConcernsShape | null>(null);
 
   return (
-    // <div className="App">
-    //   <Header />
-    //   {error ? (
-    //     <h1>{error}</h1>
-    //   ) : (
-    //     <main className="main-content">
-    //       {concerns ?
-    //       <Results concerns={concerns}/> :
-    //       <>
-    //         <h1 className="heading">Terms of Service Processor</h1>
-    //         <p className="sub-heading">Understand what's important to you.</p>
-    //         <Form setConcerns={setConcerns} setError={setError}/>
-    //       </>}
-    //     </main>
-    //   )}
-    // </div>
     <div className="App">
-      <Login />
+      <Header />
+      {error ? (
+        <h1>{error}</h1>
+      ) : (
+        <main className="main-content">
+          {concerns ?
+          <Results concerns={concerns}/> :
+          <>
+            <h1 className="heading">Terms of Service Processor</h1>
+            <p className="sub-heading">Understand what's important to you.</p>
+            <Form setConcerns={setConcerns} setError={setError}/>
+          </>}
+        </main>
+      )}
     </div>
   );
 };
