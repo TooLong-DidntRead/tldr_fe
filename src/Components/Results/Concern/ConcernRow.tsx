54306e76047f9ec3e12633b60aa9f073d9d9b9ca
Jason Kirchman <kirchman.jason@icloud.com>
2023-05-16 23:44:27 -0500

    Reshape response data and improve results CSS

--- src/Components/Results/Concern/ConcernRow.tsx
+++ src/Components/Results/Concern/ConcernRow.tsx
@@ -1,16 +1,18 @@
 import './ConcernRow.css'
 import PriorityHighRoundedIcon from '@mui/icons-material/PriorityHighRounded';
 
-interface ConcernRowProps{
+interface ConcernRowProps {
+  id: number
   title: string
   rating: number
   icon?: React.ReactNode
+  selectConcern: (id: number) => void
 }
 
-const ConcernRow = ({title, rating, icon}: ConcernRowProps) => {
+const ConcernRow = ({id, title, rating, icon, selectConcern}: ConcernRowProps) => {
 
   return (
-    <div className="concern-row">
+    <div className='concern-row' onClick={() => selectConcern(id)}>
       <p className='concern-title'>{title}</p>
       <PriorityHighRoundedIcon color='action'/>
     </div>
--- src/Components/Results/Results.tsx
+++ src/Components/Results/Results.tsx
@@ -1,26 +1,54 @@
+import { useState } from "react";
 import { ConcernsShape } from "../../interfaces";
 import ConcernRow from "./Concern/ConcernRow";
 import "./Results.css";
-
 
 interface ResultsProps {
   concerns: ConcernsShape | null;
 }
 
-const Results = ({concerns}: ResultsProps) => {
+const concernFriendly: {[key: string] : string} = {
+  "privacy": "Privacy",
+  "security": "Security",
+  "intellectualProperty": "Intellectual Property",
+  "liability": "Liability and Indemnification",
+  "cancellation": "Termination and Cancellation",
+  "payment": "Payment and Fees"
+}
 
-  const keys = concerns && Object.keys(concerns)
-  const concernRows = keys && keys.map((concern, i) => <ConcernRow key={i} title={concern} rating={10}/>);
+const Results = ({ concerns }: ResultsProps) => {
+  const [selectedConcern, setSelectedConcern] = useState(0);
+
+  const keys = concerns && Object.keys(concerns);
+
+  const selectConcern = (id: number) => {
+    setSelectedConcern(id);
+  }
+
+  const concernRows =
+    keys &&
+    keys.map((concern, i) => (
+      <ConcernRow key={i} id={i} title={concernFriendly[concern]} rating={10} selectConcern={selectConcern}/>
+    ));
 
   return (
     <div className="results-page">
       <h1 className="results-title">Your Results</h1>
       <div className="results-lower">
-        <div className="concern-rows">
-          {concernRows}
-        </div>
+        <div className="concern-rows">{concernRows}</div>
         <div className="results-card">
-          <h3 className="results-heading">{concerns && keys && concerns[keys[0]].summary}</h3>
+          <div className="results-card-header">
+            <h3>{keys && concernFriendly[keys[selectedConcern]]}</h3>
+            <h4>Consumer Friendlyness Ranking: {concerns && keys && concerns[keys[selectedConcern]].ranking}</h4>
+          </div>
+          <div className="results-card-section">
+            <h4>How Does This Impact Me?</h4>
+            <p>{concerns && keys && concerns[keys[selectedConcern]].impact}</p>
+          </div>
+          <div className="results-card-section">
+            <h4>Actionable Steps</h4>
+            <p>{concerns && keys && concerns[keys[selectedConcern]].actionable}</p>
+          </div>
         </div>
       </div>
     </div>
