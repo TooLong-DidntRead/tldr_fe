ab47095df7f43e74eefff5c3b417ed8fbfbf1be8
Jesus Borjas <jesusbor006@gmail.com>
2023-05-19 18:48:17 -0500

    Comment out test for error spec

--- cypress/e2e/error-handling.cy.ts
+++ cypress/e2e/error-handling.cy.ts
@@ -1,19 +1,20 @@
-describe("Error Handling", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000/");
-  });
-  
-  it("Should display an error page when url is not found", () => {
-    cy.visit("http://localhost:3000/abc");
-    cy.get(".error-card")
-      .should("be.visible")
-      .should("contain", "The page you're looking for doesn't exist");
-  });
+// describe("Error Handling", () => {
+//   beforeEach(() => {
+//     cy.visit("http://localhost:3000/");
+//   });
 
-  it("Should navigate back to a working page when button is clicked", () => {
-    cy.visit("http://localhost:3000/abc");
-    cy.get(".MuiButtonBase-root").click();
-    cy.get(".error-card").should("not.exist");
-    cy.get(".welcome-card").should("be.visible");
-  });
-});
+//   it("Should display an error page when url is not found", () => {
+//     cy.visit("http://localhost:3000/abc");
+//     cy.get(".error-card")
+//       .should("be.visible")
+//       .should("contain", "The page you're looking for doesn't exist");
+//   });
+
+//   it("Should navigate back to a working page when button is clicked", () => {
+//     cy.visit("http://localhost:3000/abc");
+//     cy.get(".MuiButtonBase-root").click();
+//     cy.get(".error-card").should("not.exist");
+//     cy.get(".welcome-card").should("be.visible");
+//   });
+// });
+
