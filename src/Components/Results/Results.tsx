5dcddfe9a69a3cc9af8374bd9297f94017189986
Jason Kirchman <kirchman.jason@icloud.com>
2023-05-19 00:13:40 -0500

    Add circular meter for ranking

--- src/Components/Results/Results.tsx
+++ src/Components/Results/Results.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { ConcernShape } from "../../interfaces";
 import ConcernRow from "./ConcernRow/ConcernRow";
 import "./Results.css";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 interface ResultsProps {
   concerns: ConcernShape[];
@@ -21,6 +23,16 @@ const Results = ({ concerns }: ResultsProps) => {
         />
       );
     });
+  
+  const getMeterColor = (value: number) : string => {
+    if(value <= 4) {
+      return '#D7263D';
+    }else if(value <= 7) {
+      return '#59C1BD';
+    }else {
+      return '#358600';
+    }
+  }
 
   return (
     <main className="results-main">
@@ -29,8 +41,23 @@ const Results = ({ concerns }: ResultsProps) => {
         <div className="concern-rows">{concernRows}</div>
         <div className="results-card">
           <div className="results-card-header">
-            <h3>{concerns[selectedConcern].title}</h3>
-            <h4>Ranking: {concerns[selectedConcern].ranking}</h4>
+            <h3 className="result-concern-title">
+              {concerns[selectedConcern].title}
+            </h3>
+            <div style={{ width: 80, height: 80}}>
+              <CircularProgressbar
+                value={concerns[selectedConcern].ranking}
+                maxValue={10}
+                text={`${concerns[selectedConcern].ranking}/10`}
+                styles={buildStyles({
+                  pathColor: getMeterColor(concerns[selectedConcern].ranking),
+                  textColor: '#2E2E2E',
+                  textSize: '1.3rem',
+                  trailColor: '#d6d6d6',
+                  backgroundColor: '#3e98c7',
+                })}
+              />
+            </div>
           </div>
           <div className="results-card-section">
             <h4>How Does This Impact Me?</h4>
