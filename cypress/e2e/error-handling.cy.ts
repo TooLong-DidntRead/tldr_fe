325ccf86bbf5f29f5cd3f520542153b72302a10b
Jesus Borjas <jesusbor006@gmail.com>
2023-05-19 18:33:20 -0500

    Feat: Test error page

---
+++ cypress/e2e/error-handling.cy.ts
@@ -0,0 +1,15 @@
+describe("Error Handling", () => {
+  it("Should display an error page when url is not found", () => {
+    cy.visit("http://localhost:3000/abc");
+    cy.get(".error-card")
+      .should("be.visible")
+      .should("contain", "The page you're looking for doesn't exist");
+  });
+
+  it("Should navigate back to a working page when button is clicked", () => {
+    cy.visit("http://localhost:3000/abc");
+    cy.get(".MuiButtonBase-root").click();
+    cy.get(".error-card").should("not.exist");
+    cy.get(".welcome-card").should("be.visible");
+  });
+});
