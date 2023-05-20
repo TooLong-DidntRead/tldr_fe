91c6d88c49b2d56d8bcb3dac92261093b8bf9894
Jesus Borjas <jesusbor006@gmail.com>
2023-05-19 19:51:11 -0500

    Add base url to cypress config file

--- cypress/e2e/interaction.cy.ts
+++ cypress/e2e/interaction.cy.ts
@@ -1,6 +1,6 @@
 describe("As a user, I should recieve a summary of the terms that is easy to read and understand", () => {
   beforeEach(() => {
-    cy.visit("http://127.0.0.1:3000/");
+    cy.visit("/");
     cy.get(".MuiButtonBase-root").click();
     cy.get(":nth-child(1) > #avatar-img > .MuiAvatar-img").click();
   });
