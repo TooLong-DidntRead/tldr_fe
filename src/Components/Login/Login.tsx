ffdd3dc3603bbf2656930613a8327dec92dae1d8
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-05-20 14:07:51 -0500

    feat: Add tab accessible functionality to the Login component

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,3 +1,4 @@
+import React, {KeyboardEvent} from "react";
 import "./Login.css";
 import Avatar1 from "../../images/avatar-1.jpg";
 import Avatar2 from "../../images/avatar-2.jpg";
@@ -9,6 +10,7 @@ interface LoginProps {
   setUser: React.Dispatch<React.SetStateAction<number | null>>
 }
 
+
 const Login = ({setUser}:LoginProps) => {
   let history = useHistory();
 
@@ -17,22 +19,28 @@ const Login = ({setUser}:LoginProps) => {
     setUser(id)
   }
 
+  const handleKeyPress = (event:KeyboardEvent, id:number): void => {
+    console.log(event.key)
+    if (event.key === "Enter") {
+      loginUser(id)
+    }
+  }
 
   return (
     <main className="login-main">
       <div className="login-card">
         <h3 className="login-heading">Please Select A User</h3>
         <div className="avatar-container">
-          <div className="avatar">
-            <Avatar onClick={() => loginUser(1)} id="avatar-img" alt="Travis Howard" src={Avatar1} />
+          <div className="avatar" >
+            <Avatar onClick={() => loginUser(1)} onKeyDown={(event) => handleKeyPress(event, 1)} id="avatar-img" alt="Travis Howard" src={Avatar1} tabIndex={0}/>
             <p className="avatar-name">Travis Howard</p>
           </div>
           <div className="avatar">
-            <Avatar onClick={() => loginUser(2)} id="avatar-img" alt="Cindy Baker" src={Avatar2} />
+            <Avatar onClick={() => loginUser(2)} onKeyDown={(event) => handleKeyPress(event, 2)}id="avatar-img" alt="Cindy Baker" src={Avatar2} tabIndex={0}/>
             <p className="avatar-name">Cindy Baker</p>
           </div>
           <div className="avatar">
-            <Avatar onClick={() => loginUser(3)} id="avatar-img" alt="Remy Sharp" src={Avatar3} />
+            <Avatar onClick={() => loginUser(3)} onKeyDown={(event) => handleKeyPress(event, 3)}id="avatar-img" alt="Remy Sharp" src={Avatar3} tabIndex={0}/>
             <p className="avatar-name">Remy Sharp</p>
           </div>
         </div>
