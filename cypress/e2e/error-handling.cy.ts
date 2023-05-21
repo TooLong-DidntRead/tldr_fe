d351a41293e6a1a5e7e6c4b4633d7d4dbb84a7f2
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-05-21 14:51:25 -0500

    fix: Add tsconfig.json file to cypress folder

--- cypress/e2e/error-handling.cy.ts
+++ cypress/e2e/error-handling.cy.ts
@@ -1,20 +1,20 @@
-// describe("Error Handling", () => {
-//   beforeEach(() => {
-//     cy.visit("http://localhost:3000/");
-//   });
+describe("Error Handling", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
 
-//   it("Should display an error page when url is not found", () => {
-//     cy.visit("http://localhost:3000/abc");
-//     cy.get(".error-card")
-//       .should("be.visible")
-//       .should("contain", "The page you're looking for doesn't exist");
-//   });
+  it("Should display an error page when url is not found", () => {
+    cy.visit("http://localhost:3000/abc");
+    cy.get(".error-card")
+      .should("be.visible")
+      .should("contain", "The page you're looking for doesn't exist");
+  });
 
-//   it("Should navigate back to a working page when button is clicked", () => {
-//     cy.visit("http://localhost:3000/abc");
-//     cy.get(".MuiButtonBase-root").click();
-//     cy.get(".error-card").should("not.exist");
-//     cy.get(".welcome-card").should("be.visible");
-//   });
-// });
+  it("Should navigate back to a working page when button is clicked", () => {
+    cy.visit("http://localhost:3000/abc");
+    cy.get(".MuiButtonBase-root").click();
+    cy.get(".error-card").should("not.exist");
+    cy.get(".welcome-card").should("be.visible");
+  });
+});
 
