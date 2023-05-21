2bdac29b372d07f67d04b60bf5c600bc5ac42827
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-05-21 14:59:04 -0500

    fix: Change url to hopefully resolve .visit() error in Actions

--- cypress/e2e/error-handling.cy.ts
+++ cypress/e2e/error-handling.cy.ts
@@ -1,17 +1,17 @@
 describe("Error Handling", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("/abc");
   });
 
   it("Should display an error page when url is not found", () => {
-    cy.visit("http://localhost:3000/abc");
+    // cy.visit("http://localhost:3000/abc");
     cy.get(".error-card")
       .should("be.visible")
       .should("contain", "The page you're looking for doesn't exist");
   });
 
   it("Should navigate back to a working page when button is clicked", () => {
-    cy.visit("http://localhost:3000/abc");
+    // cy.visit("http://localhost:3000/abc");
     cy.get(".MuiButtonBase-root").click();
     cy.get(".error-card").should("not.exist");
     cy.get(".welcome-card").should("be.visible");
