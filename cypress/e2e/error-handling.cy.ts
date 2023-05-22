3846865ce6754a55df5a7f75b5d9e706947a1e03
Jason Kirchman <kirchman.jason@icloud.com>
2023-05-21 22:22:16 -0500

    Change error handling to network error

--- cypress/e2e/error-handling.cy.ts
+++ cypress/e2e/error-handling.cy.ts
@@ -1,18 +1,58 @@
 describe("Error Handling", () => {
-  beforeEach(() => {
-    cy.visit("abc")
-  });
 
-  it("Should display an error page when url is not found", () => {
-    cy.get(".error-card")
-      .should("be.visible")
-      .should("contain", "The page you're looking for doesn't exist");
-  });
+  it("Should be able to handle a bad response", () => {
+    cy.intercept("POST", "https://tldr-api.onrender.com/api/v1/queries",
+      {
+        statusCode: 400
+      }
+    );
 
-  it("Should navigate back to a working page when button is clicked", () => {
+    cy.visit("")
     cy.get(".MuiButtonBase-root").click();
-    cy.get(".error-card").should("not.exist");
-    cy.get(".welcome-card").should("be.visible");
+    cy.get(":nth-child(1) > #avatar-img > .MuiAvatar-img").click();
+
+    cy.get("#tos").type(
+      `Netflix Terms of Use
+    Netflix provides a personalized subscription service that allows our members to access entertainment content ("Netflix content") over the Internet on certain Internet-connected TV's, computers and other devices ("Netflix ready devices").`,
+      { delay: 0 }
+    );
+
+    cy.get(
+      ":nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input"
+    ).click();
+    cy.get(
+      ":nth-child(4) > .MuiButtonBase-root > .PrivateSwitchBase-input"
+    ).click();
+    cy.get(
+      ":nth-child(6) > .MuiButtonBase-root > .PrivateSwitchBase-input"
+    ).click();
+
+    cy.get(".MuiButton-contained").click();
+
+    cy.get('.error-card')
+      .should("contain", "Uh-Oh, There's been an error");
   });
+
+  // beforeEach(() => {
+  //   cy.visit("abc")
+  // });
+
+  // it("Should display an error page when url is not found", () => {
+  //   cy.get(".error-card")
+  //     .should("be.visible")
+  //     .should("contain", "The page you're looking for doesn't exist");
+  // });
+
+  // it("Should display an error page when url is not found", () => {
+  //   cy.get(".error-card")
+  //     .should("be.visible")
+  //     .should("contain", "The page you're looking for doesn't exist");
+  // });
+
+  // it("Should navigate back to a working page when button is clicked", () => {
+  //   cy.get(".MuiButtonBase-root").click();
+  //   cy.get(".error-card").should("not.exist");
+  //   cy.get(".welcome-card").should("be.visible");
+  // });
 });
 
