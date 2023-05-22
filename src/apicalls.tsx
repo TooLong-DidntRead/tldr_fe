9a46ff6809d36033494b2c2fa7f3079ebb405a45
Jason Kirchman <kirchman.jason@icloud.com>
2023-05-22 16:05:23 -0500

    Add pdf uploader functionality

--- src/apicalls.tsx
+++ src/apicalls.tsx
@@ -33,4 +33,33 @@ const processTOS = async (tos: string, concerns: string[], setError: Function, u
   return response.json();
 };
 
+
+export const processTOSPDF = async (file: File, concerns: string[], setError: Function, user: number): Promise<TOSReturn> => {
+  var data = new FormData()
+  data.append('file', file)
+  concerns.forEach(concern => data.append('areas_of_focus', concern));
+  data.append('user', user.toString())
+
+  console.log(data)
+
+  const details = {
+    method: "POST",
+    headers: {},
+    body: data
+  };
+  
+  const response = await fetch(
+    "https://tldr-api.onrender.com/api/v1/queries",
+    details
+  );
+
+  if (!response.ok) {
+    const res = await response.json();
+    throw new Error(res.statusText);
+  };
+
+  return response.json();
+};
+
+
 export default processTOS;
