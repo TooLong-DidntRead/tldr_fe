727df2937190c20af4d61b52c016810e83f27f2f
Jason Kirchman <kirchman.jason@icloud.com>
2023-05-21 23:05:27 -0500

    Re-arrange error tests

--- cypress/e2e/error-handling.cy.ts
+++ cypress/e2e/error-handling.cy.ts
@@ -1,12 +1,16 @@
 describe("Error Handling", () => {
 
+  before(() => {
+    cy.wait(15000)
+  })
+
   it("Should be able to handle a bad response", () => {
     cy.intercept("POST", "https://tldr-api.onrender.com/api/v1/queries",
       {
         statusCode: 400
       }
     );
-    cy.wait(30000)
+    
     cy.visit("")
     cy.get(".MuiButtonBase-root").click();
     cy.get(":nth-child(1) > #avatar-img > .MuiAvatar-img").click();
@@ -33,17 +37,15 @@ describe("Error Handling", () => {
       .should("contain", "Uh-Oh, There's been an error");
   });
 
-  beforeEach(() => {
-    cy.visit("abc")
-  });
-
   it("Should display an error page when url is not found", () => {
+    cy.visit("abc")
     cy.get(".error-card")
       .should("be.visible")
       .should("contain", "The page you're looking for doesn't exist");
   });
 
   it("Should navigate back to a working page when button is clicked", () => {
+    cy.visit("abc")
     cy.get(".MuiButtonBase-root").click();
     cy.get(".error-card").should("not.exist");
     cy.get(".welcome-card").should("be.visible");
