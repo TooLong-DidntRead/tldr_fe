1dfa40f723b374c48a126885cf9d5acb43e2b82c
Jason Kirchman <kirchman.jason@icloud.com>
2023-05-21 22:52:46 -0500

    Change wait to 30s

--- cypress/e2e/error-handling.cy.ts
+++ cypress/e2e/error-handling.cy.ts
@@ -6,7 +6,7 @@ describe("Error Handling", () => {
         statusCode: 400
       }
     );
-    cy.wait(15000)
+    cy.wait(30000)
     cy.visit("")
     cy.get(".MuiButtonBase-root").click();
     cy.get(":nth-child(1) > #avatar-img > .MuiAvatar-img").click();
